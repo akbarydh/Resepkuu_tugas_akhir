7c66eed6d97a6e2825e7b908dc50d62737ae75ea
akbarydh <2300016085@webmail.uad.ac.id>
2024-12-20 16:43:04 +0700

    commit kedua

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AddRecipe from "./pages/AddRecipe"; // Tambahkan import untuk halaman AddRecipe
