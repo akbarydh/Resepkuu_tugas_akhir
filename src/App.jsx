50c3c65591901538b7b55413a9f5de844a3aac66
akbarydh <2300016085@webmail.uad.ac.id>
2025-01-25 21:29:03 +0700

    commiiitt

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AddRecipe from "./pages/AddRecipe";
 import RecipeDetail from "./pages/RecipeDetail";
-import EditRecipe from "./pages/EditRecipe";  // Import halaman EditRecipe
+import EditRecipe from "./pages/EditRecipe"; // Import halaman EditRecipe
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<AddRecipe />} />
-          <Route path="/recipe/:id" element={<RecipeDetail />} />
-          <Route path="/edit/:id" element={<EditRecipe />} /> {/* Route ke halaman edit resep */}
-x        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddRecipe />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/edit/:id" element={<EditRecipe />} /> {/* Route ke halaman edit resep */}
+      </Routes>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Tambahkan import BrowserRouter
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Resepkuu_tugas_akhir"> {/* Tambahkan basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
